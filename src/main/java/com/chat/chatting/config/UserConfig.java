41d2529473cf10dbf5b345b81c0ed2d6a5a01ab2
TigerKim9 <kkm9499@gmail.com>
2022-06-04 23:48:35 +0900

    login단 구현 중

---
+++ src/main/java/com/chat/chatting/config/UserConfig.java
@@ -0,0 +1,67 @@
+package com.chat.chatting.config;
+
+import java.util.Collection;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+import com.chat.chatting.socket.ChatHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class UserConfig implements UserDetails{
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final ChatHandler chatHandler;
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
